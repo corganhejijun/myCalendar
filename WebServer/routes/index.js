ef2ae3eb5eafcb7ee363409fb906a66a976971c4
corgan <corganhejijun@qq.com>
2018-05-16 01:51:18 +0800

    增加web服务器对于sqlite数据库的访问

--- WebServer/routes/index.js
+++ WebServer/routes/index.js
@@ -1,15 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var path    = require("path");
+var sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database(path.join(__dirname, "..", 'data.db'));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + '/../views/index.html'));
+  res.sendFile(path.join(__dirname, '/../views/index.html'));
 });
 
 router.get('/json', function(req, res, next){
-  var data = {name:"abcname", data:"abcdata"};
-  res.send(JSON.stringify(data));
+  var param = req.query;
+  if (param.hasOwnProperty("username")){
+    var username = param.username;
+    var passowrd = param.passowrd;
+    var sqlStr = "SELECT * FROM 'user' WHERE name='" + username + "' AND password='" + passowrd + "'";
+    console.log(sqlStr);
+    db.all(sqlStr, function(err, rows){
+      if (!err && rows.length == 1)
+        res.send(JSON.stringify({result:true}));
+      else
+        res.send(JSON.stringify({result:false, msg:err}));
+    });
+  }
 });
 
 module.exports = router;
