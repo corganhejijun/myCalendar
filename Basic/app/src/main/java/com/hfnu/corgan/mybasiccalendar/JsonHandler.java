7ee8743a43f386edd494797b110c0dfca516a5a6
corgan <corganhejijun@qq.com>
2018-05-20 00:27:46 +0800

    用户注册及登录，查询和写入数据库

--- Basic/app/src/main/java/com/hfnu/corgan/mybasiccalendar/JsonHandler.java
+++ Basic/app/src/main/java/com/hfnu/corgan/mybasiccalendar/JsonHandler.java
@@ -8,20 +8,41 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonHandler extends Handler {
+    MainActivity main;
+    public JsonHandler(MainActivity main){
+        this.main = main;
+    }
+
     @Override
     public void handleMessage(Message msg) {
         super.handleMessage(msg);
         switch (msg.what){
             case R.integer.http_get_json_success:
-                try {
-                    JSONObject obj = (JSONObject)msg.obj;
-                    Log.v("info", "name:" + obj.getString("name") + " data:" + obj.getString("data"));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+                JSONObject obj = (JSONObject)msg.obj;
+                handleJson(obj);
                 break;
             default: break;
 
+        }
+    }
+
+    void handleJson(JSONObject obj){
+        try {
+            Boolean result = obj.getBoolean("result");
+            String event = obj.getString("event");
+            if (result == false) {
+                Log.v("info", "json result false, event is " + event + ", message is " + obj.getString("msg"));
+                main.onFailed(event);
+                return;
+            }
+            if (event.equals("login")){
+                main.onLoginSuccess();
+            }
+            if (event.equals("register")){
+                main.onLoginSuccess();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 }
--- WebServer/routes/index.js
+++ WebServer/routes/index.js
@@ -13,17 +13,41 @@ router.get('/', function(req, res, next) {
 router.get('/json', function(req, res, next){
   var param = req.query;
   if (param.hasOwnProperty("username")){
-    var username = param.username;
-    var passowrd = param.passowrd;
-    var sqlStr = "SELECT * FROM 'user' WHERE name='" + username + "' AND password='" + passowrd + "'";
-    console.log(sqlStr);
-    db.all(sqlStr, function(err, rows){
-      if (!err && rows.length == 1)
-        res.send(JSON.stringify({result:true}));
-      else
-        res.send(JSON.stringify({result:false, msg:err}));
-    });
+    if (param.hasOwnProperty("register")){
+      register(res, param);
+    }
+    else{
+      login(res, param);
+    }
   }
 });
+
+function login(res, param){
+  var username = param.username;
+  var passowrd = param.password;
+  var sqlStr = "SELECT * FROM 'user' WHERE name='" + username + "' AND password='" + passowrd + "'";
+  console.log(sqlStr);
+  db.all(sqlStr, function(err, rows){
+    if (err == null && rows.length == 1)
+      res.send(JSON.stringify({event:"login", result:true}));
+    else
+      res.send(JSON.stringify({event:"login", result:false, msg:err}));
+  });
+}
+
+function register(res, param){
+  var username = param.username;
+  var passowrd = param.password;
+  var sqlStr = "INSERT INTO 'user' (name,password) VALUES ('" + username + "','" + passowrd + "')";
+  console.log(sqlStr);
+  db.run(sqlStr, function(err){
+    if (err != null){
+      res.send(JSON.stringify({event:"register", result:false, msg:err}));
+    }
+    else{
+      res.send(JSON.stringify({event:"register", result:true}));
+    }
+  })
+}
 
 module.exports = router;
